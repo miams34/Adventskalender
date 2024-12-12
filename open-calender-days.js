d3b5acbe8d5814eb19f43c5ad25db5f3fa9d9049
miams34 <mmschmidt009@gmail.com>
2024-12-12 08:50:34 +0100

    Touch Event hinzugefügt

--- open-calender-days.js
+++ open-calender-days.js
@@ -15,27 +15,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Klick-Event für Türchen
             door.addEventListener('click', function() {
+                openCalenderDoor(dayNumber, todayDay, todayMonth, door);
+            });
 
-                // Türchen nur anzeigen, wenn der Tag kleiner oder gleich dem heutigen Tag ist
-                if (dayNumber <= todayDay && todayMonth === 11) {
-                    // Zeige die Zahl
-                    const contentDiv = door.querySelector('.calender-content');
-                    const numberDiv = door.querySelector('.calender-number');
-
-                    // Wenn das Türchen noch nicht geöffnet ist, öffne es
-                    if (contentDiv.classList.contains('hidden')) {
-                        contentDiv.classList.remove('hidden');
-                        numberDiv.classList.add('hidden');
-                        return;
-                    }
-
-                    if (numberDiv.classList.contains('hidden')) {
-                        numberDiv.classList.remove('hidden');
-                        contentDiv.classList.add('hidden');
-                    }
-                    return;
-                } 
-                document.getElementById('popup').classList.remove('hidden');
+            door.addEventListener('touchend', function() {
+                openCalenderDoor(dayNumber, todayDay, todayMonth, door);
             });
     });
 });
+
+function openCalenderDoor(dayNumber, todayDay, todayMonth, door) {
+     // Türchen nur anzeigen, wenn der Tag kleiner oder gleich dem heutigen Tag ist
+     if (dayNumber <= todayDay && todayMonth === 11) {
+        // Zeige die Zahl
+        const contentDiv = door.querySelector('.calender-content');
+        const numberDiv = door.querySelector('.calender-number');
+
+        // Wenn das Türchen noch nicht geöffnet ist, öffne es
+        if (contentDiv.classList.contains('hidden')) {
+            contentDiv.classList.remove('hidden');
+            numberDiv.classList.add('hidden');
+            return;
+        }
+
+        if (numberDiv.classList.contains('hidden')) {
+            numberDiv.classList.remove('hidden');
+            contentDiv.classList.add('hidden');
+        }
+        return;
+    } 
+    document.getElementById('popup').classList.remove('hidden');
+}
